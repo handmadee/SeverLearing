545997a3d6e520c4946a67dc48eebe88ace483ce
Dino_datcoder38 <dinodatcoder38@gmail.com>
2024-05-11 00:49:26 +0700

    update domain

--- src/public/JS/config.js
+++ src/public/JS/config.js
@@ -1,3 +1,3 @@
-const LOCALHOST_API_URL = 'http://localhost:3052/v1/api/';
+const LOCALHOST_API_URL = 'https://tsmart-com.onrender.com/v1/api/';
 const LOCALHOST = 'http://localhost:3052';
 export { LOCALHOST_API_URL, LOCALHOST };
