61015e9fa19b2d7fdfb905f0d2f05f7a1098c9ea
Dino_datcoder38 <dinodatcoder38@gmail.com>
2024-07-05 01:16:29 +0700

    up

--- sever.js
+++ sever.js
@@ -1,8 +1,7 @@
 const app = require("./src/app");
 const config = require("./src/configs/config.mongodb");
-const PORT = config.app.port || 8001;
-
-const server = app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
