176636023013fb3f0cac8c04d75e0412ece34c1d
tinhph74 <tinhph74@gmail.com>
2025-05-11 17:22:55 +0700

    config

--- src/public/JS/config.js
+++ src/public/JS/config.js
@@ -1,4 +1,4 @@
 
-// const LOCALHOST_API_URL = 'https://app-learing-tsmart-c0c598e19be5.herokuapp.com/v1/api/';
-const LOCALHOST_API_URL = 'http://localhost:3052/v1/api/'
+ const LOCALHOST_API_URL = 'https://app-learing-tsmart-c0c598e19be5.herokuapp.com/v1/api/';
+//const LOCALHOST_API_URL = 'http://localhost:3052/v1/api/'
 export { LOCALHOST_API_URL };
